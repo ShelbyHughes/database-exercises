29353a629b882f640f64da155bb660acb13526ce
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-02-19 11:45:22 -0600

    Finished tables exercises

---
+++ albums_migration.sql
@@ -0,0 +1,16 @@
+-- Create the albums database
+CREATE DATABASE IF NOT EXISTS codeup_test_db;
+-- Use the database
+USE codeup_test_db;
+-- If table already exist, delete it
+DROP TABLE IF EXISTS albums;
+-- Create new table called albums
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(255) NOT NULL,
+    name  VARCHAR(255) NOT NULL,
+    release_date INT UNSIGNED NOT NULL,
+    sales DOUBLE NOT NULL,
+    genre VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
