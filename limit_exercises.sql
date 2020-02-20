4eac99b28f822dbe49b6b01ed0ebd48cd44b591e
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-02-20 16:18:23 -0600

    function exercises

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -2,3 +2,5 @@ USE employees;
 SELECT DISTINCT last_name FROM employees GROUP BY last_name DESC LIMIT 10;
 SELECT * FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND birth_date LIKE '%-12-25' ORDER BY birth_date ASC, hire_date DESC LIMIT 5;
 SELECT * FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND birth_date LIKE '%-12-25' ORDER BY birth_date ASC, hire_date DESC LIMIT 5 OFFSET 45;
+
+SELECT DISTINCT first_name, last_name, COUNT(*) as cnt FROM employees GROUP BY first_name, last_name ORDER BY cnt DESC;
