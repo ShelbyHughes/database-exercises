58fed0bea969c236d7512278d34e3bbec0c5748d
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-02-24 14:19:53 -0600

    ...

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,9 +1,9 @@
 USE employees;
-SELECT first_name, last_name, hire_date FROM employees WHERE hire_date = ( SELECT hire_date FROM employees WHERE emp_no=101010 );
+SELECT first_name, last_name, hire_date FROM employees WHERE hire_date = (SELECT hire_date FROM employees WHERE emp_no=101010);
 
-SELECT e.first_name, e.last_name, t.title FROM employees e , titles t WHERE e.emp_no = t.emp_no AND  e.first_name = 'Aamod';
+SELECT DISTINCT title FROM titles WHERE emp_no IN(SELECT emp_no FROM employees WHERE first_name = 'Aamod');
 
-SELECT e.first_name AS 'first_name', e.last_name AS 'last_name' FROM employees e , dept_manager dm WHERE e.emp_no = dm.emp_no AND e.gender = 'F' AND dm.to_date = '9999-01-01';
+SELECT first_name, last_name FROM employees WHERE gender = 'F' AND emp_no IN (SELECT emp_no FROM dept_manager WHERE to_date = '9999-01-01');
 
 SELECT d.dept_name AS 'dept_name' FROM departments d, dept_manager dm, employees e WHERE e.emp_no = dm.emp_no AND dm.dept_no = d.dept_no AND e.gender = 'F' and dm.to_date = '9999-01-01' GROUP BY d.dept_name;
 
